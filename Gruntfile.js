b16eff20751d6ba7ec2cd4f0386dec7cb2ec2eaf
Loan Chau <chauthingocloan0309@gmail.com>
2025-03-18 23:31:02 +0700

    add uglify task to Gruntfile.js and update package.json for devDependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,15 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        // Task configuration goes here
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'src/<%= pkg.name %>.js',
+                dest: 'build/<%= pkg.name %>.min.js'
+            }
+        }
     });
 
     // Load the plugin that provides the "uglify" task.
